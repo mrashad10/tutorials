2fb88991ae98cc4f22916c44262ce4a3d119bbc5
Mohamad Rashad <mrashad@mrashad.com>
2023-09-29 18:43:25 +0300

    feat(PHP): Add the Stock Manager

---
+++ php-stock/logout.php
@@ -0,0 +1,11 @@
+<?php 
+
+require_once 'php_action/core.php';
+
+// remove all session variables
+session_unset(); 
+
+// destroy the session 
+session_destroy(); 
+
+header('location: http://localhost:9080/stock/index.php');
